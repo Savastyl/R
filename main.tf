96db2390a7665f1a561eb4562fa0994026de93f8
Savas <savastyludf@hotmail.com>
2022-12-17 23:08:18 -0500

    var.rg_location

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "savas_rg" {
   name     = var.rg_name
-  location = "West Europe"
+  location = var.rg_location
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,7 @@ variable "rg_name" {
     type = string
   
 }
+variable "rg_location" {
+    default = "West Europe"
+  
+}
